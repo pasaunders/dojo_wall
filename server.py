d41e8ffa385a9fb4aeac38bfd237d55c668a89c8
Patrick <kaorti@gmail.com>
2017-09-14 18:53:11 -0700

    working registration login and messages, comments in progress

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
+from sqlalchemy import exc
 import re
+import md5
+import os
+import binascii
 
 app = Flask(__name__)
 app.secret_key = "supersecret"
@@ -16,22 +20,60 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST'])
 def login():
     """Allow users to log in.
 
     Validate credentials against user in database and save user id to session.
     Then redirect to the wall.
     """
+    email = request.form['email']
+    password = request.form['password']
+    user_query = "SELECT * FROM users WHERE users.email = :email LIMIT 1"
+    query_data = {'email': email}
+    user = mysql.query_db(user_query, query_data)
+    if len(user) > 0:
+        encrypted_password = md5.new(password + user[0]['salt']).hexdigest()
+        if user[0]['password'] == encrypted_password:
+            session['id'] = user[0]['id']
+            return redirect('/wall')
+    else:
+        return redirect('/')
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST'])
 def register():
     """Register new users.
 
     Validate registration form input. Add valid form entries to the database.
     Then log the registered user in and redirect to wall.
     """
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    confirm = request.form['confirm']
+    salt = binascii.b2a_hex(os.urandom(15))
+    hashed_pw = md5.new(password + salt).hexdigest()
+    if len(name) > 0 and len(email) > 7 and len(password) > 0 and password == confirm and EMAIL_REGEX.match(email):
+        query = 'INSERT INTO users (name, email, password, created_at, updated_at, salt) VALUES (:name, :email, :password, NOW(), NOW(), :salt)'
+        data = {
+            'name': name,
+            'email': email,
+            'password': hashed_pw,
+            'salt': salt,
+        }
+        try:
+            session['id'] = mysql.query_db(query, data)
+            return redirect('/wall')
+        except exc.IntegrityError:
+            flash('email already used')
+            return redirect('/')
+    elif not EMAIL_REGEX.match(email) or not len(email) > 7:
+        flash('Please submit a valid email')
+        return redirect('/')
+    else:
+        flash('All form fields must be filled out')
+        return redirect('/')
 
 
 @app.route('/wall')
@@ -39,20 +81,43 @@ def wall():
     """Display the wall page.
 
     The wall shows posts in descending chronological order. Users may post new
-    messages and comment on existing messages. Message comments are also displayed in
-    descending chron order.
+    messages and comment on existing messages. Message comments are also
+    displayed in descending chron order.
     """
-    return render_template('wall.html')
+    messages = mysql.query_db('SELECT users.name, messages.id, messages.message, messages.created_at, messages.updated_at FROM messages JOIN users ON messages.users_id = users.id;')
+    return render_template('wall.html', messages=messages)
 
 
-@app.route('/post')
+@app.route('/post', methods=['POST'])
 def post():
     """Post a message on the wall."""
+    message = request.form['message']
+    if len(message) > 0:
+        query = "INSERT INTO messages (users_id, message, created_at, updated_at) VALUES (:user_id, :message, NOW(), NOW())"
+        data = {
+            'user_id': session['id'],
+            'message': message,
+        }
+        mysql.query_db(query, data)
+    return redirect('/wall')
 
 
-@app.route('/comment')
+
+@app.route('/comment', methods=['POST'])
 def comment():
     """Comment on an existing message."""
+    comment = request.form['comment']
+    import pdb; pdb.set_trace()
+    messages_id = request.form['messages_id']
+    if len(comment) > 0:
+        query = "INSERT INTO comments (users_id, messages_id, comment, created_at, updated_at) VALUES (:user_id, :messages_id, :comment, NOW(), NOW())"
+        data = {
+            'user_id': session['id'],
+            'comment': comment,
+            'messages_id': messages_id,
+        }
+        mysql.query_db(query, data)
+    return redirect('/wall')
 
 
 app.run(debug=True)
